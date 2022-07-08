9be1c5f9af48569ebd9b27325607ed30e3dbc542
Johnny Allen <johnny.allen@daveramsey.com>
2022-07-07 22:27:38 -0500

    Add code through chapter 13

---
+++ src/main/java/Bank.java
@@ -0,0 +1,13 @@
+public class Bank {
+    Money reduce(Expression source, String to) {
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
+
+    public void addRate(String fromCurrency, String toCurrency, int rate) {
+
+    }
+}
---
+++ src/main/java/Money.java
@@ -0,0 +1,42 @@
+class Money implements Expression {
+    protected int amount;
+    protected String currency;
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount/rate, to);
+    }
+}
